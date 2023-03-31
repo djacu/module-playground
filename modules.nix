43529c8311a0239e076478404a706714c200918d
Daniel Baker <daniel.baker@tweag.io>
2023-03-30 18:03:45 -0700

    use apply attribute so we don't have to convert to string later.

--- modules.nix
+++ modules.nix
@@ -4,24 +4,26 @@
 in {
   options.zpoolprops = {
     ashift = lib.mkOption {
-        type = lib.types.int;
         default = 0;
+        type = lib.types.int;
+        apply = builtins.toString;
     };
     autotrim = lib.mkOption {
         type = lib.types.enum ["on" "off"];
         default = "off";
+        apply = builtins.toString;
     };
     output = lib.mkOption {
         type = lib.types.str;
+        internal = true;
     };
   };
 
   config.zpoolprops = {
     output = let
       cleanCfg = removeAttrs cfg [ "output" ];
-      stringCfg = lib.mapAttrs (name: value: builtins.toString value) cleanCfg;
-      setCfg = lib.mapAttrsToList (name: value: name + "=" + value) stringCfg;
+      setCfg = lib.mapAttrsToList (name: value: name + "=" + value) cleanCfg;
     in
-      lib.concatMapStrings (x: "-o " + x + " \\\n") setCfg;
+      lib.concatMapStrings (x: "    -o " + x + " \\\n") setCfg;
   };
 }
