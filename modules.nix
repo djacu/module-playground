216345c6599adbf0824082f202cc2d4b89a2a978
Daniel Baker <daniel.baker@tweag.io>
2023-04-01 11:26:55 -0700

    finished zpoolprops

--- modules.nix
+++ modules.nix
@@ -3,26 +3,67 @@
   types = lib.types;
 in {
   options.zpoolprops = {
+    altroot = lib.mkOption {
+        type = types.nullOr types.path;
+        default = null;
+    };
     ashift = lib.mkOption {
+        type = types.int;
         default = 0;
-        type = lib.types.int;
-        apply = builtins.toString;
+    };
+    autoexpand = lib.mkOption {
+        type = types.enum ["on" "off"];
+        default = "off";
+    };
+    autoreplace = lib.mkOption {
+        type = types.enum ["on" "off"];
+        default = "off";
     };
     autotrim = lib.mkOption {
-        type = lib.types.enum ["on" "off"];
+        type = types.enum ["on" "off"];
         default = "off";
-        apply = builtins.toString;
     };
+    bootfs = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+    };
+    cachefile = lib.mkOption {
+        type = types.either types.path types.string;
+        default = "none";
+    };
+    compatibility = lib.mkOption {
+        type = types.either (types.enum ["off" "legacy"]) types.path;
+        default = "off";
+    };
+    delegation = lib.mkOption {
+        type = types.enum ["on" "off"];
+        default = "on";
+    };
+    failmode = lib.mkOption {
+        type = types.enum ["wait" "continue" "panic"];
+        default = "wait";
+    };
+
+    listsnapshots = lib.mkOption {
+        type = types.enum ["on" "off"];
+        default = "off";
+    };
+    multihost = lib.mkOption {
+        type = types.enum ["on" "off"];
+        default = "off";
+    };
+
     output = lib.mkOption {
-        type = lib.types.str;
+        type = types.str;
         internal = true;
     };
   };
 
   config.zpoolprops = {
     output = let
-      cleanCfg = removeAttrs cfg [ "output" ];
-      setCfg = lib.mapAttrsToList (name: value: name + "=" + value) cleanCfg;
+      cleanCfg = lib.filterAttrs (name: value: value != null) (removeAttrs cfg [ "output" ]);
+      stringCfg = lib.mapAttrs (name: value: builtins.toString value) cleanCfg;
+      setCfg = lib.mapAttrsToList (name: value: name + "=" + value) stringCfg;
     in
       lib.concatMapStrings (x: "    -o " + x + " \\\n") setCfg;
   };
